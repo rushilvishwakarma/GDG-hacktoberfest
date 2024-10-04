00a8c40e18db26497fb3485f1934d59d92e50960
Rushil <rushil.edu@gmail.com>
2024-10-04 17:04:32 +0530

    added new header logo

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -118,7 +118,7 @@ export function SiteHeader() {
       <div className="ml-auto flex h-full items-center justify-end">
          <img
             src="/Institutions_Innovation_Council.png"
-            className="text-primary h-16 w-20 max-w-none max-h-none"
+            className="text-primary h-18 w-22 max-w-none max-h-none"
             alt="Institutions Innovation Council Logo"
          />
       </div>
